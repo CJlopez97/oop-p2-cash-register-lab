4676ec1637f3c521dcb8160978cbcb6637eeee59
Cristian J Lopez-Ramos <crisjossua@gmail.com>
2026-09-19 22:31:50 -0600

    Cash Register assignment is complete

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,56 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+        self.discount = discount
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if isinstance(value, int) and 0 <= value <= 100:
+            self._discount = value
+        else:
+            print("Not valid discount")
+
+    def add_item(self, item, price, quantity=1):
+        cost = price * quantity
+        self.total += cost
+
+        for _ in range(quantity):
+            self.items.append(item)
+
+        transaction = {
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        }
+        self.previous_transactions.append(transaction)
+
+    def apply_discount(self):
+        if not self.discount:
+            print("There is no discount to apply.")
+            return
+
+        discount_amount = self.total * (self.discount / 100)
+        self.total -= discount_amount
+
+        # Format integer values if there are no decimals to match $800 requirement
+        formatted_total = int(self.total) if self.total.is_integer() else self.total
+        print(f"After the discount, the total comes to ${formatted_total}.")
+
+    def void_last_transaction(self):
+        if not self.previous_transactions:
+            print("There is no transaction to void.")
+            return
+
+        last_transaction = self.previous_transactions.pop()
+        cost = last_transaction["price"] * last_transaction["quantity"]
+        self.total -= cost
+
+        for _ in range(last_transaction["quantity"]):
+            if last_transaction["item"] in self.items:
+                self.items.remove(last_transaction["item"])
